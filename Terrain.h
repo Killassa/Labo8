83c2edf6f2c17a886e2e74a78f09ca26c12f5a25
Cedro22 <cedric.rosat@me.com>
2022-01-19 01:03:14 +0100

    Ajout de commentaires + TODO

--- Terrain.h
+++ Terrain.h
@@ -5,11 +5,18 @@ Nom du labo     : Labo 8 - Survivor
 Auteur(s)       : Grégory Rey-Mermet, Cédric Rosat
 Date creation   : 14.01.2022
 
-Description     : Ce fichier d'en-tête (.h) met à la disposition de l'utilisateur
-                  diverses fonctions utiles...
+Description     : Cette classe permet d'instancier un terrain pouvant contenir des
+                  objets pour autant que ces derniers aient une coordonnée (voir
+                  classe coordonnée). Ajoute aussi certaines fonctions pour interagir
+                  avec le terrain et son fonctionnement.
 
 Remarque(s)     : L'opérateur de flux << peut afficher correctement au maximum 9
                   objets.
+                  Chaque objet doit avoir comme propriété une coordonnée (voir classe
+                  coordonnée)
+                  //TODO Ajout de l'appel d'initRand() ?
+                  Si la méthode nouvel objet est utilisée, il faut initialiser la
+                  seed pour la fonction rand().
 
 Modification(s) : -
 
@@ -20,8 +27,8 @@ Compilateur     : Mingw-w64 g++ 11.2.0
 #ifndef TERRAIN_H
 #define TERRAIN_H
 
-#include <vector>
-#include <ostream>
+#include <vector>  // Vecteur d'objets
+#include <ostream> // Flux de sortie
 
 template <typename T>
 using Objets = std::vector<T>;
@@ -29,31 +36,44 @@ using Objets = std::vector<T>;
 template <typename T>
 class Terrain {
    /**
+    *  Affiche le terrain ainsi que les objets dessus
     *
-    * @param os
-    * @param terrain
+    * @param os      Flux courant
+    * @param terrain Terrain à afficher
     */
    friend std::ostream& operator<< <T>(std::ostream& os, const Terrain<T>& terrain);
 
 public:
+   /* -------------------------------------------------------------------------------
+    *  Constructeurs et destructeur
+    * -----------------------------------------------------------------------------*/
+
    /**
     * Constructeur d'initialisation
     *
-    * @param largeur
-    * @param hauteur
+    * @param largeur Largeur du terrain
+    * @param hauteur Hauteur du terrain
     */
    Terrain(unsigned largeur, unsigned hauteur);
+
+   //TODO Destructeur implicite pas suffisant, destruction en profondeur
+   // Si le terrain est détruit, tous les objets sont détruits avec
+
+   /* -------------------------------------------------------------------------------
+    *  Fonctions membres
+    * -----------------------------------------------------------------------------*/
 
    /**
     * Ajoute un objet sur le terrain
     *
-    * @param objet
+    * @param objet Objet à ajouter sur le terrain
     */
    void ajoutObjet(const T& objet);
 
    /**
+    * Créé un nouvel objet et lui attribue des coordonnées aléatoires uniques
     *
-    * @return
+    * @return Retourne un objet avec des coordonnées uniques
     */
    T nouvelObjet();
 
@@ -66,20 +86,23 @@ public:
    void deplacerObjets(unsigned distance = 1);
 
    /**
-    * Supprime un objet présent si un autre objet vient se positionner sur lui
+    * Supprime un objet du terrain (vecteur d'objets)
     */
    void supprimerObjets();
 
-   /**
-    * Indique si le déplacement des objets dans le terminé, lorsqu'il ne reste plus
-    * qu'un objet non détruit dans la liste d'objets du terrain
+   /** //TODO Adapter le commentaire ci-dessous en fonction de l'autre TODO
+    * Indique si le nombre d'objets présents sur le terrain est <= 1
     *
-    * @return   True  : Il ne reste qu'un objet dans la liste d'objets
+    * @return   True  : Il ne reste un objet ou moins dans la liste d'objets
     *           False : Il reste plusieurs objets dans la liste d'objets
     */
    bool estTermine();
 
 private:
+   /* -------------------------------------------------------------------------------
+    *  Données membres
+    * -----------------------------------------------------------------------------*/
+
    const unsigned _largeur, _hauteur;
    Objets<T>      _objets;
 };
--- Terrain_def.h
+++ Terrain_def.h
@@ -5,7 +5,8 @@ Nom du labo     : Labo 8 - Survivor
 Auteur(s)       : Grégory Rey-Mermet, Cédric Rosat
 Date creation   : 14.01.2022
 
-Description     : Ce fichier définit diverses fonctions utiles...
+Description     : Définit toutes les fonctions, opérateurs ou constructeurs
+                  nécessaires pour interagir avec le terrain et les objets.
 
 Remarque(s)     : -
 
@@ -17,26 +18,27 @@ Compilateur     : Mingw-w64 g++ 11.2.0
 #ifndef TERRAIN_DEF_H
 #define TERRAIN_DEF_H
 
-#include <string>
-#include <iostream>
-#include <iomanip>
+#include <string>   // Utilisation des strings
+#include <iostream> // Flux de sortie et cout
+#include <iomanip>  // setw() et setfill()
 
 #include "annexe.h"
-
-using namespace std;
 
 /* -------------------------------------------------------------------------------
  *  Déclaration des fonctions internes
  * -----------------------------------------------------------------------------*/
 
 /**
- *
+ * Affiche la limite verticale du terrain
  *
  * @tparam T
  * @param largeur
+ * @param caractere
  */
+ //TODO Ne devrait pas être générique car la largeur est unisgned de toute façon
+ // mais ça nécessite un fichier .cpp uniquement pour cette fonction...
 template <typename T>
-void afficherLimiteVert(T largeur);
+void afficherLimiteVert(T largeur, char caractere = '-');
 
 
 /* -------------------------------------------------------------------------------
@@ -45,12 +47,12 @@ void afficherLimiteVert(T largeur);
 
 //TODO Revoir l'algorithmie pour économiser la mémoire et être plus efficace
 template <typename T>
-ostream& operator<<(ostream& os, const Terrain<T>& terrain) {
+std::ostream& operator<<(std::ostream& os, const Terrain<T>& terrain) {
    afficherLimiteVert<unsigned>(terrain._largeur + 2);
 
    for (unsigned ligne = 0; ligne < terrain._hauteur; ++ligne) {
       // String représentant une ligne sans objets
-      string contenu(terrain._largeur, ' ');
+      std::string contenu(terrain._largeur, ' ');
 
       auto first = terrain._objets.cbegin();
 
@@ -59,14 +61,14 @@ ostream& operator<<(ostream& os, const Terrain<T>& terrain) {
                              [ligne](const T& obj){return obj.getCoordonnee().getPosY() == ligne;}))
             != terrain._objets.cend()) {
          // Remplace les " " par l'ID du robot (len = nb de digits de l'ID)
-         string id = to_string(first->_id);
+         std::string id = to_string(first->_id);
          contenu.replace(first->getCoordonnee().getPosX(), id.length(), id);
 
          // Continue la recherche après l'occurrence en cours
          ++first;
       }
       // Affiche la ligne avec les bordures et les robots
-      os << "|" << contenu << "|" << endl;
+      os << "|" << contenu << "|" << std::endl;
    }
    afficherLimiteVert<unsigned>(terrain._largeur + 2);
    return os;
@@ -88,33 +90,35 @@ void Terrain<T>::ajoutObjet(const T& objet) {
    _objets.push_back(objet);
 }
 
+//TODO nouvelObjet implique la copie d'un objet -> coordAleatoires pour économiser
+// de la mémoire ?
 template <typename T>
 T Terrain<T>::nouvelObjet() {
 
    // Impossible de créer plus d'objets qu'il n'y a de cases
    assert(_objets.size() < _largeur * _hauteur);
 
-   Coordonnee coordonnee;
+   Coordonnee coord;
 
    // Contrôle si la position de l'objet ne recouvre aucun autre objet
    do {
-      // Générations de positions aléatoires dans les limites du terrain
-      coordonnee = Coordonnee(nbreAleatoire(_largeur), nbreAleatoire(_hauteur));
+      // Génération de positions aléatoires dans les limites du terrain
+      coord = Coordonnee(nbreAleatoire(_largeur), nbreAleatoire(_hauteur));
 
       // Pas de contrôle de recouvrement s'il y a moins de 2 objets
       if ((_objets).size() < 2) {break;}
 
    } while(any_of((_objets).cbegin(), (_objets).cend(),
-                  [coordonnee](const T& elem){return coordonnee == elem.getCoordonnee();}));
+                  [coord](const T& obj){return coord == obj.getCoordonnee();}));
 
-   T objet(coordonnee);
+   T objet(coord);
 
    return objet;
 }
 
 template <typename T>
 void Terrain<T>::deplacerObjets(unsigned distance) {
-   unsigned positionInitiale = 0;
+   unsigned posInitiale = 0;
 
    for(T& objet : _objets) {
       if (objet.getEstDetruit()) continue;
@@ -124,11 +128,12 @@ void Terrain<T>::deplacerObjets(unsigned distance) {
 
       do {
          //Prend une direction parmi toutes les directions possibles
-         direction = (Coordonnee::Direction)nbreAleatoire((int)Coordonnee::Direction::LEFT + 1);
+         direction = (Coordonnee::Direction)
+                      nbreAleatoire((int) Coordonnee::Direction::LEFT + 1);
 
          switch (direction) {
             case Coordonnee::Direction::UP:
-               estDeplacable = objet.getCoordonnee().getPosY() - distance > positionInitiale;
+               estDeplacable = objet.getCoordonnee().getPosY() - distance > posInitiale;
                break;
             case Coordonnee::Direction::DOWN:
                estDeplacable = objet.getCoordonnee().getPosY() + distance < _hauteur;
@@ -137,7 +142,7 @@ void Terrain<T>::deplacerObjets(unsigned distance) {
                estDeplacable = objet.getCoordonnee().getPosX() + distance < _largeur;
                break;
             case Coordonnee::Direction::LEFT:
-               estDeplacable = objet.getCoordonnee().getPosX() - distance > positionInitiale;
+               estDeplacable = objet.getCoordonnee().getPosX() - distance > posInitiale;
                break;
          }
       }while(!estDeplacable);
@@ -146,7 +151,7 @@ void Terrain<T>::deplacerObjets(unsigned distance) {
 
       //Destruction de l'objet précédemment présent
       for(T& aDetruire : _objets) {
-         if (aDetruire.getEstDetruit() || aDetruire.getId() == objet.getId())
+         if (aDetruire.getEstDetruit() or aDetruire.getId() == objet.getId())
             continue;
 
          if (aDetruire.getCoordonnee() == objet.getCoordonnee()) {
@@ -166,7 +171,8 @@ void Terrain<T>::supprimerObjets() {
 
 template <typename T>
 bool Terrain<T>::estTermine() {
-   return _objets.size() == 1;
+   return _objets.size() == 1; //TODO <= 1 si jamais 2 robots sont supprimés en même
+                               // temps
 }
 
 
@@ -175,8 +181,8 @@ bool Terrain<T>::estTermine() {
  * -----------------------------------------------------------------------------*/
 
 template <typename T>
-void afficherLimiteVert(T largeur) {
-   cout << setfill('-') << setw(largeur + 1) << '\n';
+void afficherLimiteVert(T largeur, char caractere) {
+   std::cout << std::setfill(caractere) << setw(largeur + 1) << '\n';
 }
 
 #endif //TERRAIN_DEF_H
